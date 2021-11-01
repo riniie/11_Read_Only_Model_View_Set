ff80e4fd94debc397a9dde153ab6c1dc338e4ee9
rini <itsrini16@gmail.com>
2021-11-01 16:34:03 +0545

    Read Only Model ViewSet

---
+++ project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from api.views import StudentModelViewSet
+
+router = DefaultRouter()
+router.register('student', StudentModelViewSet, basename='student')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls))
+]
